6168c6ab01f3c9bac20dc24657b4116ce992f172
askolik <andrea.skolik@gmail.com>
2021-07-16 12:28:08 +0200

    CCP

--- src/envs/cartpole/classical/plots/cp_plot_classical.py
+++ src/envs/cartpole/classical/plots/cp_plot_classical.py
@@ -9,24 +9,24 @@ path = '../../../../../' + BASE_PATH
 
 hps = {
     'learning_rate': 0.001,
-    'update_after': 5, 'update_target_after': 10, 'batch_size': 16}
+    'update_after': 1, 'update_target_after': 1, 'batch_size': 32}
 plot_avg_vals(
     'scores', 5000, 10,
      path + 'cartpole_classical/params_790/', 'update 1, 1', 'g', hps)
 
-hps = {
-    'learning_rate': 0.0001,
-    'update_after': 5, 'update_target_after': 10, 'batch_size': 16}
-plot_avg_vals(
-    'scores', 5000, 10,
-     bak_path + 'cartpole_classical/params_790/', 'update 1, 1', 'g', hps)
-
-hps = {
-    'learning_rate': 0.001,
-    'update_after': 5, 'update_target_after': 10, 'batch_size': 64}
-plot_avg_vals(
-    'scores', 5000, 10,
-     path + 'cartpole_classical/params_790/', 'update 5, 10', 'r', hps)
+# hps = {
+#     'learning_rate': 0.0001,
+#     'update_after': 5, 'update_target_after': 10, 'batch_size': 16}
+# plot_avg_vals(
+#     'scores', 5000, 10,
+#      bak_path + 'cartpole_classical/params_790/', 'update 1, 1', 'g', hps)
+#
+# hps = {
+#     'learning_rate': 0.001,
+#     'update_after': 5, 'update_target_after': 10, 'batch_size': 64}
+# plot_avg_vals(
+#     'scores', 5000, 10,
+#      path + 'cartpole_classical/params_790/', 'update 5, 10', 'r', hps)
 
 # hps = {
 #     'learning_rate': 0.001,
